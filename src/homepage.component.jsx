9efb56e4eca7ee749e9b4165c32b3f6a60b0dd56
RR David <aso.rd55@gmail.com>
2020-04-05 14:46:41 +1000

    edit homepage component file

--- src/homepage.component.jsx
+++ src/homepage.component.jsx
@@ -14,25 +14,25 @@ const HomePage = () => (
             </div>	
             <div className = 'menu-item'>	
                 <div className = 'content'>	
-                    <div className ='title'> HATS </div>	
+                    <div className ='title'> SHOES </div>	
                     <span className = 'subtitle'> SHOP NOW </span>	
                 </div>	
             </div>	
             <div className = 'menu-item'>	
                 <div className = 'content'>	
-                    <div className ='title'> HATS </div>	
+                    <div className ='title'> JACKETS </div>	
                     <span className = 'subtitle'> SHOP NOW </span>	
                 </div>	
             </div>	
             <div className = 'menu-item'>	
                 <div className = 'content'>	
-                    <div className ='title'> HATS </div>	
+                    <div className ='title'> WOMENS </div>	
                     <span className = 'subtitle'> SHOP NOW </span>	
                 </div>	
             </div>	
             <div className = 'menu-item'>	
                 <div className = 'content'>	
-                    <div className ='title'> HATS </div>	
+                    <div className ='title'> MENS </div>	
                     <span className = 'subtitle'> SHOP NOW </span>	
                 </div>	
             </div>	
