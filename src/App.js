244a7b6828fba2c2a342759bf2d72cd40c6c1b08
RR David <aso.rd55@gmail.com>
2020-07-04 19:16:57 +1000

    update files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import HomePage from './pages/homepage/homepage.component.jsx'
-import {Route} from 'react-router-dom'
+import {Route, Switch} from 'react-router-dom'
+import ShopPage from './pages/shop/shop.component.jsx'
 
 import './App.css';
 
@@ -8,8 +9,11 @@ import './App.css';
 
 function App() {
   return (
-    <div>  
+    <div> 
+      <Switch>
     <Route  exact = 'true' path = '/' component = {HomePage}   />
+    <Route path = '/shop' component = {ShopPage} />
+    </Switch> 
     </div>
   );
 }
